3bcc56bb655230428970cf63cbe7446b14b34f3b
bitbreakr <yan.philippe@me.com>
2025-11-03 11:21:35 +0100

    refactor(router): rename Context to RouterContext and HandlerFunc to RouterFuncHandler for improved naming consistency

--- v1/router.go
+++ v1/router.go
@@ -1,7 +1,7 @@
 package plugin
 
 // Context represents an abstraction for handling HTTP requests and responses.
-type Context interface {
+type RouterContext interface {
 	// JSON sends a JSON response with the specified status code and body.
 	JSON(status int, body any) error
 
@@ -16,21 +16,21 @@ type Context interface {
 }
 
 // HandlerFunc defines a function type for handling HTTP requests using a context and returning an error, if any.
-type HandlerFunc func(ctx Context) error
+type RouterFuncHandler func(ctx RouterContext) error
 
 // Router defines methods for routing HTTP requests.
 type Router interface {
 	// Get registers a handler for HTTP GET requests to the specified``` pathgo.
-	Get(path string, handler HandlerFunc)
+	Get(path string, handler RouterFuncHandler)
 
 	// Post registers a handler for HTTP POST requests to the specified path.
-	Post(path string, handler HandlerFunc)
+	Post(path string, handler RouterFuncHandler)
 
 	// Put registers a handler for HTTP PUT requests to the specified path.
-	Put(path string, handler HandlerFunc)
+	Put(path string, handler RouterFuncHandler)
 
 	// Delete registers a handler for HTTP DELETE requests to the specified path.
-	Delete(path string, handler HandlerFunc)
+	Delete(path string, handler RouterFuncHandler)
 
 	// Group creates a new router group with a specified prefix and returns the Router instance.
 	Group(prefix string) Router
