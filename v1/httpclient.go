be53b853fd4853f8202658cabf608ac674795fcc
bitbreakr <yan.philippe@me.com>
2025-11-03 10:21:30 +0100

    refactor(httpclient): enhance interface definitions with extended comments and add new HTTP methods for flexibility

--- v1/httpclient.go
+++ v1/httpclient.go
@@ -6,26 +6,33 @@ import (
 	"time"
 )
 
+// HTTPCLientSetters defines methods for configuring and performing HTTP client operations.
 type HTTPCLientSetters interface {
 	// SetBaseURL sets the base URL for the HTTP client.
 	SetBaseURL(base string) HTTPClient
+
 	// SetHeader sets a header for the HTTP client.
 	SetHeader(key, value string) HTTPClient
+
 	// SetTimeout sets the timeout for the HTTP client.
 	SetTimeout(d time.Duration) HTTPClient
+
 	// SetMaxRetries sets the maximum number of retries for the HTTP client.
 	SetMaxRetries(n int) HTTPClient
+
 	// SetBackoff sets the backoff duration for the HTTP client.
 	SetBackoff(d time.Duration) HTTPClient
+
 	// SetLogger sets the logger for the HTTP client.
 	SetLogger(l Logger) HTTPClient
+
 	// Get sends an HTTP GET request to the specified URL with optional headers and unmarshals the response into out.
 	Get(ctx context.Context, url string, out any, headers map[string]string) (*http.Response, error)
 }
 
+// HttpClientMethods defines HTTP client methods for sending requests with various HTTP methods.
 type HttpClientMethods interface {
-	// Post sends an HTTP POST request to the specified URL with the given body,
-	// headers, and decodes the response into out.
+	// Post performs an HTTP POST request with context, URL, body, response decoding, and headers.
 	Post(
 		ctx context.Context,
 		url string,
@@ -33,6 +40,8 @@ type HttpClientMethods interface {
 		out any,
 		headers map[string]string,
 	) (*http.Response, error)
+
+	// Put performs an HTTP PUT request with context, URL, body, response decoding, and headers.
 	Put(
 		ctx context.Context,
 		url string,
@@ -40,6 +49,8 @@ type HttpClientMethods interface {
 		out any,
 		headers map[string]string,
 	) (*http.Response, error)
+
+	// Delete performs an HTTP DELETE request with context, URL, response decoding, and headers.
 	Delete(
 		ctx context.Context,
 		url string,
@@ -47,6 +58,7 @@ type HttpClientMethods interface {
 		headers map[string]string,
 	) (*http.Response, error)
 
+	// DoRequest performs a general HTTP request with a specified method, URL, body, response decoding, and headers.
 	DoRequest(
 		ctx context.Context,
 		method, url string,
