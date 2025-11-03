5f8f4bc609f30e0d1f5ee21c4716f55b64ed1acc
bitbreakr <yan.philippe@me.com>
2025-11-03 10:14:49 +0100

    refactor(plugin): add detailed comments to types, interfaces, and constants for clarity

--- v1/deployerplugin.go
+++ v1/deployerplugin.go
@@ -4,6 +4,7 @@ package plugin
 type GetAffectedDeploymentsResponse struct {
 	// Service represents the identifier of the affected service in the deployment response.
 	Service string `json:"service"`
+
 	// CreatedAtTime indicates the timestamp when the affected deployment was created, formatted as a string.
 	CreatedAtTime string `json:"createdAt"`
 }
@@ -12,10 +13,13 @@ type GetAffectedDeploymentsResponse struct {
 type DeployContext struct {
 	// Name of the deployment.
 	Service string
+
 	// Image of the deployment.
 	Image string
+
 	// SetStatus sets the status of the deployment using the provided DeploymentStatus value.
 	SetStatus func(status DeploymentStatus)
+
 	// SetRevisionURL sets the revision URL of the deployment.
 	RevisionURL *string
 }
@@ -26,10 +30,13 @@ type DeploymentStatus string
 const (
 	// DeploymentStatusPending indicates that the deployment is awaiting execution or processing.
 	DeploymentStatusPending DeploymentStatus = "pending"
+
 	// DeploymentStatusSuccess indicates that the deployment was successful.
 	DeploymentStatusSuccess DeploymentStatus = "success"
+
 	// DeploymentStatusInProgress indicates that the deployment is in progress.
 	DeploymentStatusInProgress DeploymentStatus = "progress"
+
 	// DeploymentStatusFailed indicates that the deployment failed.
 	DeploymentStatusFailed DeploymentStatus = "failed"
 )
@@ -38,9 +45,12 @@ const (
 type DeployerPluginCommands interface {
 	// Deploy performs the deployment of a service based on the provided DeployContext and returns an error if it fails.
 	Deploy(ctx *DeployContext) error
+
 	// GetRunningServices retrieves a list of currently running services and returns an error if the operation fails.
 	GetRunningServices() ([]any, error)
-	// GetAffectedRevisions retrieves a list of affected deployments for the given service name and returns an error if it fails.
+
+	// GetAffectedRevisions retrieves a list of affected deployments for the given
+	// service name and returns an error if it fails.
 	GetAffectedRevisions(name string) (result []GetAffectedDeploymentsResponse, err error)
 }
 
